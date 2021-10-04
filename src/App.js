9ea6529cefee5f598b1f371e83ba01bb3822cea8
Batuhan Balta <bthn.balta@gmail.com>
2021-10-04 01:19:48 -0700

    axios

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from "react";
+
 import WorldMap from 'react-svg-worldmap';
 import serializer from "./components/Countries";
-import CountriesCapital from "./components/CountriesCapital"
+// import CountriesCapital from './components/CountriesCapital';
+import axios from "axios";
+import {useEffect,useState} from "react";
 
-console.log(CountriesCapital());
 
 
-// console.log(serializer())
+
+
+
+
+
 function App() {
-  const data = serializer();
+  
 
+  const [countries,setCountries] = useState([]);
+
+
+  useEffect(() =>{
+      axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response=>setCountries(response.data))
+      
+  },[])
+  const tata = serializer();
+ 
   return (
+
+    
     <div className="App">
       <WorldMap
         color="red"
         title="Capital Guess Game"
         value-suffix="people"
         size="lg"
-        data={data}
+        data={tata}
       />
+    {/* <CountriesCapital/> */}
+    
+      
     </div>
   );
 }
--- src/components/CountriesCapital.js
+++ src/components/CountriesCapital.js
@@ -1,17 +1,25 @@
 import axios from "axios";
 import {useEffect,useState} from "react";
-const CountriesCapital = ()=>{
+
+
+export default function CountriesCapital(){
     const [countries,setCountries] = useState([]);
 
 
     useEffect(() =>{
-        axios.get('https://api.countrylayer.com/v2/')
-        .then(response=>console.log('Axios then',response))
-    },[])
-
-    return{
+        axios
+        .get('https://restcountries.com/v3.1/all')
+        .then(response=>setCountries(response.data))
         
-    }
+    },[])
+    
+    return(
+        <div key="zaza">
+          {countries.map(country=>{
+              return(
+                  <h1>{country.name.common}:{country.capital}</h1>
+              )
+          })}  
+        </div>
+    )
 }
-
-export default CountriesCapital;
